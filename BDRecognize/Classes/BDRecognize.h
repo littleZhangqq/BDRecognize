15c2d87c990d405d59c4cca798433b4976972413
[892750407@qq.com] <[892750407@qq.com]>
2019-12-28 13:37:29 +0800

    daa

---
+++ BDRecognize/Classes/BDRecognize.h
@@ -0,0 +1,16 @@
+
+//
+//  Header.h
+//  yqjy
+//
+//  Created by admin on 2019/11/16.
+//  Copyright © 2019 易起. All rights reserved.
+//
+
+#ifndef Header_h
+#define Header_h
+
+#import <AipOcrSdk/AipOcrSdk.h>
+
+
+#endif /* Header_h */
